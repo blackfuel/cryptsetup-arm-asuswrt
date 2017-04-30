69c951dac6b62f64d31e798f66769ab37bc31b68
blackfuel <blackfuel@github.com>
2017-04-30 08:37:30 -0400

    fix util-linux configure

--- cryptsetup.sh
+++ cryptsetup.sh
@@ -130,9 +130,14 @@ LIBS="-lm -lncursesw -L$TOP/ncurses/lib -L$SYSROOT/usr/lib" \
 --enable-shared \
 --enable-static \
 --disable-rpath \
---disable-silent-rules --disable-makeinstall-chown --disable-makeinstall-setuid --with-sysroot=$SYSROOT \
+--disable-silent-rules \
+--disable-bash-completion \
+--disable-makeinstall-chown \
+--disable-makeinstall-setuid \
+--with-sysroot=$SYSROOT \
 --disable-agetty \
---without-ncurses --without-ncursesw
+--without-ncurses \
+--without-ncursesw
 
 $MAKE
 make install
